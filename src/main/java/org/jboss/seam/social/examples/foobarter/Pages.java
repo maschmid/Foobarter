a44cf326812d2c7cd93cdbf139a024e7ace6c0aa
Marek Schmidt <fregaham@gmail.com>
2012-01-15 17:42:14 +0100

    update style

--- src/main/java/org/jboss/seam/social/examples/foobarter/Pages.java
+++ src/main/java/org/jboss/seam/social/examples/foobarter/Pages.java
@@ -1,5 +1,6 @@
 package org.jboss.seam.social.examples.foobarter;
 
+import org.jboss.seam.faces.security.AccessDeniedView;
 import org.jboss.seam.faces.security.LoginView;
 import org.jboss.seam.faces.view.config.ViewConfig;
 import org.jboss.seam.faces.view.config.ViewPattern;
@@ -14,8 +15,11 @@ public interface Pages {
         @LoggedIn
         WALL,
         
-        @ViewPattern("/user.xhtml")
+        @ViewPattern("/twitter_user.xhtml")
+        @LoggedIn
         @Follower
+        @LoginView("/home.xhtml")
+        @AccessDeniedView("/denied.xhtml")
         USER
     }
 }
